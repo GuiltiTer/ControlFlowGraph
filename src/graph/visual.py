171888897e6ab0b5e3bb974c9ed18e0f59abc4eb
Mojtaba Babayi <babayi.mojtaba.mb@gmail.com>
2021-12-25 17:39:13 +0330

    Add verbose option and for lineno only graphs contents

--- src/graph/visual.py
+++ src/graph/visual.py
@@ -8,12 +8,12 @@ from src.graph.utils import head_node, last_node
 FONT_SIZE = "22"
 
 
-def draw_CFG(graph, filename, token_stream, format="png"):
+def draw_CFG(graph, filename, token_stream, format="png", verbose=True):
     gr = gv.Digraph(comment=filename, format=format, node_attr={"shape": "none"})
     gr.node("start", style="filled", fillcolor="#aaffaa", shape="oval", fontsize=FONT_SIZE)
 
     for node, args in list(graph.nodes.data())[:-1]:
-        block_contents = stringify_block(token_stream, args)
+        block_contents = (stringify_block(token_stream, args) if verbose else stringify_block_lineno_only(args))
         gr.node(str(node), label=build_node_template(node, block_contents))
     gr.node(str(last_node(graph)), label="end", style="filled", fillcolor="#ffaaaa", shape="oval", fontsize=FONT_SIZE)
 
@@ -24,9 +24,8 @@ def draw_CFG(graph, filename, token_stream, format="png"):
     gr.render(f"{filename}.gv", view=True)
 
 
-def build_node_template(node_label, node_contents):
-    b = node_content_to_html(node_contents)
-    b_len = len(node_contents)
+def build_node_template(node_label, contents):
+    b_len = len(contents.splitlines())
     line_height = 40
     s = f"""<<FONT POINT-SIZE="{FONT_SIZE}">  
         <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
@@ -37,7 +36,7 @@ def build_node_template(node_label, node_contents):
             </tr>
             <tr>
                 <td width="30" height="{b_len * line_height}" fixedsize="true" sides="tlb"></td>
-                <td width="50" height="{b_len * line_height}" fixedsize="false" sides="bt" PORT="here">{b}</td>
+                <td width="50" height="{b_len * line_height}" fixedsize="false" sides="bt" PORT="here">{contents}</td>
                 <td width="30" height="{b_len * line_height}" fixedsize="true" sides="brt"></td>
             </tr>
         </TABLE>
@@ -55,4 +54,15 @@ def node_content_to_html(node_contents):
 
 
 def stringify_block(token_stream, node_args):
-    return [(rule.start.line, extract_exact_text(token_stream, rule)) for rule in node_args["data"]]
+    cs = [(rule.start.line, extract_exact_text(token_stream, rule)) for rule in node_args["data"]]
+    b = node_content_to_html(cs)
+    return b
+
+
+def stringify_block_lineno_only(node_args):
+    data = node_args["data"]
+    left, right = data[0].start.line, data[-1].stop.line
+    if left == right:
+        return f"{left}"
+
+    return f"{left}..{right}"
